38c331723365805476b0f1141872ae15850b28ef
ngoanhvce130192 <48663990+ngoanhvce130192@users.noreply.github.com>
2019-03-19 20:35:58 +0700

    Commit PriceModel

--- ProjectFinal_Group06/src/main/java/Price.java
+++ ProjectFinal_Group06/src/main/java/Price.java
@@ -1,3 +1,4 @@
+
 /**
  *
  * @author Dang Buu Hoa
@@ -10,50 +11,75 @@ public class Price {
     private Double productPrice;
     private Double VAT;
     private Double promotion;
-    
+    private Integer productId;
+
     //Constructor
-    public Price(Double productPrice, Double VAT, Double promotion) throws PriceException {
+    public Price(Integer productId, Double productPrice, Double VAT, Double promotion) throws PriceException {
         setProductPrice(productPrice);
         setVAT(VAT);
         setPromotion(promotion);
+        setProductId(productId);
     }
-    
+    //Get product id
+    public Integer getProductId() throws PriceException {
+        if (productId == null) {
+            throw new PriceException("Cannot get the product's id");
+        }
+        return productId;
+    }
+    //Set product id
+    public void setProductId(Integer productId) throws PriceException {
+        if (!productId.toString().chars().allMatch(Character::isDigit)) {
+            throw new PriceException("Only accept numbers");
+        }
+        this.productId = productId;
+    }
+
     //get product Price
     public Double getProductPrice() throws PriceException {
-        if (productPrice == null) throw new PriceException("Cannot get the product price");
+        if (productPrice == null) {
+            throw new PriceException("Cannot get the product price");
+        }
         return productPrice;
     }
-    
+
     //set product Price
     public void setProductPrice(Double productPrice) throws PriceException {
-        if (!productPrice.toString().chars().allMatch(Character::isDigit))
+        if (!productPrice.toString().chars().allMatch(Character::isDigit)) {
             throw new PriceException("Only accept numbers");
+        }
         this.productPrice = productPrice;
     }
-    
+
     //get VAT
     public Double getVAT() throws PriceException {
-        if (VAT == null) throw new PriceException("Cannot get the VAT");
+        if (VAT == null) {
+            throw new PriceException("Cannot get the VAT");
+        }
         return VAT;
     }
-    
+
     //set VAT
     public void setVAT(Double VAT) throws PriceException {
-        if (!VAT.toString().chars().allMatch(Character::isDigit))
+        if (!VAT.toString().chars().allMatch(Character::isDigit)) {
             throw new PriceException("Only accept numbers");
+        }
         this.VAT = VAT;
     }
-    
+
     //get promotion
     public Double getPromotion() throws PriceException {
-        if (promotion == null) throw new PriceException("Cannot get the product promotion");
+        if (promotion == null) {
+            throw new PriceException("Cannot get the product promotion");
+        }
         return promotion;
     }
-    
+
     //set promotion
     public void setPromotion(Double promotion) throws PriceException {
-        if (!promotion.toString().chars().allMatch(Character::isDigit))
+        if (!promotion.toString().chars().allMatch(Character::isDigit)) {
             throw new PriceException("Only accept numbers");
+        }
         this.promotion = promotion;
     }
 }
