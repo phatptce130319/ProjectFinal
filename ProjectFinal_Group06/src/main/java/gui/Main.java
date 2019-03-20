d1330be33b3d5603efb4e243df4c4b76324532ba
Phat PT <38425809+GrexPTP@users.noreply.github.com>
2019-03-20 18:56:54 +0700

    Update layout

--- ProjectFinal_Group06/src/main/java/gui/Main.java
+++ ProjectFinal_Group06/src/main/java/gui/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/mainScreen.fxml"));
+        primaryStage.setTitle("Agricultural products manager");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
